4f9448a99bfedd6a4416382e3b8595297dc22b2c
jonasrothfuss <jonas.rothfuss@gmx.de>
2017-09-23 01:28:52 +0200

    Article_Node model

--- survey/models.py
+++ survey/models.py
@@ -9,3 +9,14 @@ import glob
 from pprint import pprint
 
 fs = FileSystemStorage(location="static")
+
+class Keyword(models.Model):
+  keyword_name = models.CharField(max_length=200)
+
+class Article_Node(models.Model):
+  level = models.IntegerField()
+  title = models.CharField(max_length=200)
+  keywords = models.ManyToManyField(Keyword, blank=True, null=True)
+  text = models.CharField(max_length=5000)
+  dependent_node = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
+
